da4bb87e36a6d846b0223746a9dedf3fae0cb64e
sbx_user1051 <sbx_user1051@169.254.116.45>
2024-06-12 19:56:36 +0000

    Wed Jun 12 19:56:36 UTC 2024

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,9 +1,13 @@
 import sys
-if "D:\runner\runner-for-python-windows\accelerate_runner\lib" not in sys.path:
-    sys.path.append("D:\runner\runner-for-python-windows\accelerate_runner\lib")
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../../lib'))
+
+
 from solutions.SUM import sum_solution
 
 
 class TestSum():
     def test_sum(self):
         assert sum_solution.compute(1, 2) == 3
+
